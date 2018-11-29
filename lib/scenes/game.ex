caad9be0e88d9a3218f4f14e19917dc49bc8fd44
Merouane Atig <merwan@patem.net>
2018-11-29 21:57:42 +0100

    Draw snake

--- lib/scenes/game.ex
+++ lib/scenes/game.ex
@@ -2,23 +2,50 @@ defmodule ElixirSnake.Scene.Game do
   use Scenic.Scene
 
   alias Scenic.Graph
+  alias Scenic.ViewPort
 
-  import Scenic.Primitives, only: [text: 3]
+  import Scenic.Primitives, only: [rrect: 3, text: 3]
 
   @graph Graph.build(font: :roboto, font_size: 36)
   @tile_size 32
+  @tile_radius 8
+  @snake_starting_size 5
 
   def init(_arg, opts) do
     viewport = opts[:viewport]
 
+    # Get the view port width and height
+    {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} = ViewPort.info(viewport)
+
+    # calculate number of tiles
+    vp_tile_width = trunc(vp_width / @tile_size)
+    vp_tile_height = trunc(vp_height / @tile_size)
+
+    # start with snake centered
+    snake_start_coords = {
+      trunc(vp_tile_width / 2),
+      trunc(vp_tile_height / 2),
+    }
+
     state = %{
       viewport: viewport,
+      tile_width: vp_tile_width,
+      tile_height: vp_tile_height,
       graph: @graph,
       score: 0,
+      # Game objects
+      objects: %{
+        snake: %{
+          body: [snake_start_coords],
+          size: @snake_starting_size,
+          direction: {1, 0},
+        }
+      }
     }
 
     state.graph
     |> draw_score(state.score)
+    |> draw_game_objects(state.objects)
     |> push_graph
 
     {:ok, state}
@@ -27,5 +54,22 @@ defmodule ElixirSnake.Scene.Game do
   defp draw_score(graph, score) do
     graph
     |> text("Score: #{score}", fill: :white, translate: {@tile_size, @tile_size})
+  end
+
+  defp draw_game_objects(graph, object_map) do
+    Enum.reduce(object_map, graph, fn {object_type, object_data}, graph ->
+      draw_object(graph, object_type, object_data)
+    end)
+  end
+
+  defp draw_object(graph, :snake, %{body: snake}) do
+    Enum.reduce(snake, graph, fn {x, y}, graph ->
+      draw_tile(graph, x, y, fill: :lime)
+    end)
+  end
+
+  defp draw_tile(graph, x, y, opts) do
+    tile_opts = Keyword.merge([fill: :white, translate: {x * @tile_size, y * @tile_size}], opts)
+    graph |> rrect({@tile_size, @tile_size, @tile_radius}, tile_opts)
   end
 end
