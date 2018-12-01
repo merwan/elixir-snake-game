44aff094ad3b9d8e46083dc4d683a6154b02e3a1
Merouane Atig <merwan@patem.net>
2018-12-01 14:58:50 +0100

    Move snake

--- lib/scenes/game.ex
+++ lib/scenes/game.ex
@@ -7,9 +7,10 @@ defmodule ElixirSnake.Scene.Game do
   import Scenic.Primitives, only: [rrect: 3, text: 3]
 
   @graph Graph.build(font: :roboto, font_size: 36)
+  @frame_ms 192
   @tile_size 32
   @tile_radius 8
-  @snake_starting_size 5
+  @snake_starting_size 3
 
   def init(_arg, opts) do
     viewport = opts[:viewport]
@@ -27,11 +28,15 @@ defmodule ElixirSnake.Scene.Game do
       trunc(vp_tile_height / 2),
     }
 
+    {:ok, timer} = :timer.send_interval(@frame_ms, :frame)
+
     state = %{
       viewport: viewport,
       tile_width: vp_tile_width,
       tile_height: vp_tile_height,
       graph: @graph,
+      frame_count: 1,
+      frame_timer: timer,
       score: 0,
       # Game objects
       objects: %{
@@ -49,6 +54,30 @@ defmodule ElixirSnake.Scene.Game do
     |> push_graph
 
     {:ok, state}
+  end
+
+  def handle_info(:frame, %{frame_count: frame_count} = state) do
+    state = move_snake(state)
+
+    state.graph
+    |> draw_game_objects(state.objects)
+    |> draw_score(state.score)
+    |> push_graph
+
+    {:noreply, %{state | frame_count: frame_count + 1}}
+  end
+
+  defp move_snake(%{objects: %{snake: snake}} = state) do
+    [head | _] = snake.body
+    new_head_pos = move(state, head, snake.direction)
+
+    new_body = Enum.take([new_head_pos | snake.body], snake.size)
+
+    put_in(state, [:objects, :snake, :body], new_body)
+  end
+
+  defp move(%{tile_width: w, tile_height: h}, {pos_x, pos_y}, {vec_x, vec_y}) do
+    {rem(pos_x + vec_x + w, w), rem(pos_y + vec_y + h, h)}
   end
 
   defp draw_score(graph, score) do
